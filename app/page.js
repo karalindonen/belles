26a99517742c7bf455bc0591ae538182de225783
Caitlin Keogh <ckeogh2006@gmail.com>
2026-04-28 12:38:41 -0400

    made music page

--- app/page.js
+++ app/page.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 import Hero from "./hero";
 import About from "./about";
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+/*import { GoogleOAuthProvider } from '@react-oauth/google';
 
 root.render (
   <GoogleOAuthProvider clientId="688636205022-k972ehmf7espdltolq1gjbj6hn01m9r6.apps.googleusercontent.com">
     <Home />
   </GoogleOAuthProvider>
 
-);
+);*/
 
 export default function Home(){
   return(
