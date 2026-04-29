83f7c7f290d136bd88cb1b32429a224fc7b02274
karalindonen <soccerlefty25.com>
2026-04-29 16:19:56 -0400

    fixed typos

--- app/page.js
+++ app/page.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import Image from "next/image";
 import Hero from "./hero";
-/*import { GoogleOAuthProvider } from '@react-oauth/google';
-
-root.render (
-  <GoogleOAuthProvider clientId="688636205022-k972ehmf7espdltolq1gjbj6hn01m9r6.apps.googleusercontent.com">
-    <Home />
-  </GoogleOAuthProvider>
-
-);*/
 
 export default function Home(){
   return(
