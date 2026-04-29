73cf47b0f1b6b4966de45e587139b16e516c2038
mayaharner <mayaharner9@gmail.com>
2026-04-29 16:06:01 -0400

    added to performances page and edited database

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,6 +1,10 @@
 export default function Contact () {
     return (
-        <div>
+        <div className="contact-page">
+            <h2>Contact Us</h2>
+            <h3>General</h3>
+            <p><strong>Executive Director: Katie Koslow</strong>
+            kkoslow@villanova.edu</p>
             
         </div>
     );
--- app/performances/page.js
+++ app/performances/page.js
@@ -1,7 +1,48 @@
 export default function Performances () {
     return (
-        <div>
+        <main className="performances-page">
+            <h1>Performances</h1>
+
+            <p>Each semester, the Villanova Belles perform at numerous 
+                events. Here are some of our performances from our first semester!</p>
             
-        </div>
+            <section>
+                <div>
+                    <h2>Spring Concert 2026</h2>
+                    <p><strong>Date:</strong>April 12, 2026</p>
+                    <p>
+                        We had our first semester concert where performed all of our semester
+                         repertoire. We were fortunate to perform alongside the Haveners and Supernovas.
+                    </p>
+                </div>
+
+                <div>
+                    <h2>Admitted Students Day 2026</h2>
+                    <p><strong>Date:</strong>April 11, 2026</p>
+                    <p>Performed "No Scrubs" and "You're Still The One" at the Involvement Fair.</p>
+                </div>
+
+                <div>
+                    <h2>NOVAdance 2026</h2>
+                    <p><strong>Date:</strong>March 28, 2026</p>
+                    <p>Performed "No Scrubs" at NOVAdance at Cabrini!</p>
+                </div>
+
+                <div>
+                    <h2>A Cappella Palooza 2026</h2>
+                    <p><strong>Date:</strong>March 20, 2026</p>
+                    <p>
+                        First Palooza and Villanova debut with all the a cappella groups!! Performed 
+                        our A Cappella Palooza setlist of "No Scrubs", "So Easy (To Fall In Love)", and "Heart Attack".
+                    </p>
+                </div>
+
+                <div>
+                    <h2>Women's Basketball Game</h2>
+                    <p><strong>Date:</strong>January 18, 2026</p>
+                    <p>Our debut as a group! We performed the National Anthem for the Women's Basketball Game.</p>
+                </div>
+            </section>
+        </main>
     );
 }
